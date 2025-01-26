d2667c7373ae013a14084fdf236aa905fb3aa01e
alex.vashenkov2018@gmail.com <alex.vashenkov2018@gmail.com>
2025-01-26 02:25:26 +0200

    -added message for staging

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_sanchezz.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -14,7 +14,6 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
-        throw new Exception("Testing Insights");
     }
 }
 
